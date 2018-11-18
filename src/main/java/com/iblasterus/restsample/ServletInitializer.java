f0160535f8a3076af65dc16cb2e54036837d9b1a
IBlasterus <iblasterus@gmail.com>
2018-11-18 21:56:59 +0200

    First attempt to start application

---
+++ src/main/java/com/iblasterus/restsample/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.iblasterus.restsample;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestSampleApplication.class);
+	}
+
+}
