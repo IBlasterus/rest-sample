a11a77ae5621ccedbb0037a7b734f73cd0541b7e
IBlasterus <iblasterus@gmail.com>
2018-11-29 17:06:52 +0200

    Trying FilterRegistrationBean

--- src/main/java/com/iblasterus/restsample/AppConfig.java
+++ src/main/java/com/iblasterus/restsample/AppConfig.java
@@ -1,4 +1,9 @@
 package com.iblasterus.restsample;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+    public FilterRegistrationBean
 }
